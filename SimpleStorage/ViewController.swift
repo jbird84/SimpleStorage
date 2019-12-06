df9ce4576971b42c0c4972463023f2e9b85cf8f4
Josh Kinney <joshkinney@kinneykare.com>
2019-12-05 20:41:36 -0600

    Added a delete button (used  StackView) and implemented a function that SAFELY deletes the stored data in UserDefaults.

--- SimpleStorage/ViewController.swift
+++ SimpleStorage/ViewController.swift
@@ -9,12 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var coolLevelField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var coolLevelLabel: UILabel!
+    
+    let storedName = UserDefaults.standard.object(forKey: "name")
+    let storedCoolLevel = UserDefaults.standard.object(forKey: "coolLevel")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //        let storedName = UserDefaults.standard.object(forKey: "name")
+        //        let storedCoolLevel = UserDefaults.standard.object(forKey: "coolLevel")
+        
+        if let theName = storedName as? String {
+            if let theCoolLevel = storedCoolLevel as? String {
+                nameLabel.text = theName
+                coolLevelLabel.text = theCoolLevel
+            }
+        }
     }
-
-
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        
+        if let theName = nameField.text {
+            if let theCoolLevel = coolLevelField.text {
+                
+                //Save to UserDefaults
+                UserDefaults.standard.set(theName, forKey: "name")
+                UserDefaults.standard.set(theCoolLevel, forKey: "coolLevel")
+                
+                //set the labels
+                nameLabel.text = theName
+                coolLevelLabel.text = theCoolLevel
+            }
+        }
+    }
+    
+    
+    @IBAction func deleteButtonTapped(_ sender: Any) {
+        
+        if storedName as? String != nil {
+            UserDefaults.standard.removeObject(forKey: "name")
+            nameLabel.text = "Name:"
+        }
+        
+        if storedCoolLevel as? String != nil {
+            UserDefaults.standard.removeObject(forKey: "coolLevel")
+            coolLevelLabel.text = "Cool Level:"
+        }
+    }
 }
 
